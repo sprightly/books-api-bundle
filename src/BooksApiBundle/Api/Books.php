f9529e1d80fc120bcebcd25f3eb6ddff8d8c021c
Serj Bielanovskyi <kievswebdev@gmail.com>
2017-12-19 18:25:35 +0200

    - cache response for 10m

--- src/BooksApiBundle/Api/Books.php
+++ src/BooksApiBundle/Api/Books.php
@@ -4,22 +4,35 @@ namespace BooksApiBundle\Api;
 use Franjid\ApiWrapperBundle\Api\Api;
 use Franjid\ApiWrapperBundle\Api\ApiResponse;
 use Franjid\ApiWrapperBundle\Api\ApiRequest;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class Books extends Api implements BooksInterface
 {
     const ROUTE = 'books';
+
     const REQUEST_PARAMETER_LIMIT = 'limit';
     const REQUEST_PARAMETER_OFFSET = 'offset';
     const RESPONSE_PARAMETER_STATUS = 'status';
+
     const RESPONSE_PARAMETER_DATA = 'data';
     const RESPONSE_PARAMETER_DATA_BOOKS = 'books';
     const SUCCESS_STATUS_CODE = 'OK';
+
+    /**
+     * \DateInterval specification string
+     */
+    const CACHE_TTL = 'P10M';
 
     /**
      * {@inheritdoc}
      */
     public function getBooks($limit = null, $offset = null)
     {
+        $books = $this->getFromCache($limit, $offset);
+        if ($books) {
+            return $books;
+        }
+
         /** @var ApiRequest $request */
         $request = new ApiRequest('GET', $this::ROUTE);
         $request->setOptions($this->prepareOptions($limit, $offset));
@@ -32,10 +45,48 @@ class Books extends Api implements BooksInterface
             && $this::SUCCESS_STATUS_CODE === $response[$this::RESPONSE_PARAMETER_STATUS]
             && isset($response[$this::RESPONSE_PARAMETER_DATA][$this::RESPONSE_PARAMETER_DATA_BOOKS])
         ) {
-            return $response[$this::RESPONSE_PARAMETER_DATA][$this::RESPONSE_PARAMETER_DATA_BOOKS];
+            $books = $response[$this::RESPONSE_PARAMETER_DATA][$this::RESPONSE_PARAMETER_DATA_BOOKS];
+            $this->putInCache($limit, $offset, $books);
+
+            return $books;
         } else {
             return false;
         }
+    }
+
+    /**
+     * @param $limit
+     * @param $offset
+     * @return bool|mixed
+     */
+    private function getFromCache($limit, $offset)
+    {
+        $cache = new FilesystemAdapter();
+        $requestHash = md5($limit . $offset);
+
+        $books = $cache->getItem('books.' . $requestHash);
+        if ($books->isHit()) {
+            return $books->get();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $limit
+     * @param $offset
+     * @param $books
+     */
+    private function putInCache($limit, $offset, $books)
+    {
+        $cache = new FilesystemAdapter();
+        $requestHash = md5($limit . $offset);
+
+        $cachedBooks = $cache->getItem('books.' . $requestHash);
+        $cachedBooks->set($books);
+        $cachedBooks->expiresAfter(new \DateInterval('P10M'));
+
+        $cache->save($cachedBooks);
     }
 
     /**
